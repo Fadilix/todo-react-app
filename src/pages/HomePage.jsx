b2bb4494e4e891e3a64a4764e562244f9bada500
Fadilix <iamfadile2@gmail.com>
2024-02-11 19:43:40 +0000

    started todo interface

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,41 @@
 import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
+import capitalize from "../utils/UsualFunctions";
 
 const HomePage = () => {
   const currentDate = new Date();
+  const options = {
+    weekday: "long",
+  };
+
+  const dayString = currentDate.toLocaleString("en-US", options);
+  const dayInt = currentDate.toLocaleString("en-US", { day: "2-digit" });
+  const monthString = currentDate.toLocaleString("en-US", { month: "long" });
   useEffect(() => {
     console.log(currentDate);
   }, []);
+
   return (
     <div>
-      <Navbar />
+      <div
+        style={{
+          height: "75px",
+          marginBottom: "20px",
+        }}
+      >
+        <Navbar />
+      </div>
       <div className="todos">
-        <div className="date"></div>
+        <div className="date-nbtasks">
+          <div>
+            <h2>
+              {capitalize(dayString)}, {dayInt} {currentDate.getFullYear()}
+            </h2>
+            <p>{monthString}</p>
+          </div>
+
+          <p>12</p>
+        </div>
       </div>
     </div>
   );
---
+++ src/utils/UsualFunctions.js
@@ -0,0 +1,7 @@
+const capitalize = (str) => {
+    const capitalizedStr = str.charAt(0).toUpperCase() + str.slice(1);
+    return capitalizedStr;
+}
+
+
+export default capitalize;
