066a861361428f373dcc3e8c4ee8e1dd88ea2c7b
Fadilix <iamfadile2@gmail.com>
2024-02-11 19:25:07 +0000

    added style to the navbr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../scss/Navbar.scss";
 
 const Navbar = () => {
   return (
     <div>
-     <button></button>
+     <nav>
+      <h1>Todo App</h1>
+      <ul>
+        <li><Link to={"/"}>Home</Link></li>
+        <li><Link to={"/"}>Home</Link></li>
+        <li><Link to={"/"}>Home</Link></li>
+        <li><Link to={"/"}>Home</Link></li>
+      </ul>
+     </nav>
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Navbar from "../components/Navbar";
 
 const HomePage = () => {
+  const currentDate = new Date();
+  useEffect(() => {
+    console.log(currentDate);
+  }, []);
   return (
     <div>
-        
+      <Navbar />
+      <div className="todos">
+        <div className="date"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
